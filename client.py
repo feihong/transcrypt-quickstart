6666f584aadfdfde0a5dd044586e77b5162f2987
Feihong Hsu <feihong.hsu@gmail.com>
2016-08-15 15:40:13 -0500

    Add generator example

--- client.py
+++ client.py
@@ -1,3 +1,4 @@
+from org.transcrypt.stubs.browser import __pragma__
 import random
 
 
@@ -23,6 +24,16 @@ def plog(text):
     return jq('<p>').text(text).appendTo('#output')
 
 
+# def on_click(selector):
+#     def wrapper(fn):
+#         print(selector)
+#         print(fn)
+#         jq(selector).on('click', fn)
+#         return fn
+#
+#     return wrapper
+
+
 def clear(evt):
     jq('#output').empty()
 jq('button.clear').on('click', clear)
@@ -32,3 +43,13 @@ def hello(evt):
     text = random.choice(GREETINGS)
     plog(text)
 jq('button.hello').on('click', hello)
+
+
+def generator():
+    def fn(n):
+        for i in range(n):
+            yield i, i*i
+    for x in fn(10):
+        plog(x)
+
+jq('.generator').on('click', generator)
